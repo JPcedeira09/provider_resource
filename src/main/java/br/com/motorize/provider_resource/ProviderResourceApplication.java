e7c8f781c5765f56fe4c01867541e8c12474e1c0
Joao Paulo <joao.paulo@hvarconsulting.com.br>
2018-11-28 22:08:03 -0200

    Config Eureka
    Pronto para subir o EUREKA

--- src/main/java/br/com/motorize/provider_resource/ProviderResourceApplication.java
+++ src/main/java/br/com/motorize/provider_resource/ProviderResourceApplication.java
@@ -1,9 +1,13 @@
 package br.com.motorize.provider_resource;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableAutoConfiguration
 public class ProviderResourceApplication {
 
 	public static void main(String[] args) {
